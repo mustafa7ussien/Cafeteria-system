835eba26e611c6c79062c52b13df2498d550af4c
CoDeBrEaKe <mohamedmedhat7533@icloud.com>
2022-09-03 01:54:04 +0200

    Order and products table added and , users order relationship added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrdersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::resource("products", ProductController::class);
 Route::resource("categories", CategoryController::class);
+Route::resource("orders" , OrdersController::class );
